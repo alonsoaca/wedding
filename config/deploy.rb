0a747d83f78fa304d028e3e420ecfe4e543d0f25
Alonso <alonso.aca@gmail.com>
2012-11-19 22:12:40 -0600

    Changes to cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "wedding"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, true
+set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:bloodyaca/#{application}.git"
