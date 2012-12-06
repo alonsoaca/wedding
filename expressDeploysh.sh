07c9e2405c1d258cb28f10897fcb7fea3d63ae19
Alonso <alonso.aca@gmail.com>
2012-12-05 22:22:49 -0600

    Changed the express deploy to require commit message

--- expressDeploysh.sh
+++ expressDeploysh.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Express deploy to server using capistrano
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
 
+[ "$#" -eq 1 ] || die "Need at least commit message, $# provided"
 
 echo "Will start deployment of: $1"
 
